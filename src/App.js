7540dca868411f05ac8027082c673085224375c5
Gagan <gagank2199@gmail.com>
2023-07-19 14:28:06 +0530

    react-js JSX

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+   <nav className="nvbar">
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Help</li>
+    </ul>
+   </nav>
+   <div> 
+    <p>This is paragraph</p>
+   </div>
+
+   </>
+  );
+}
+
+export default App;
