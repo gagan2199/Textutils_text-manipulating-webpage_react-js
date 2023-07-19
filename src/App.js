ed112fb507bd1cbc810f3430e327ade64ba2c4df
Gagan <gagank2199@gmail.com>
2023-07-20 01:22:57 +0530

    lower case to upper case

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
    <Navbar title="Textutils" about="About Us"></Navbar>
+   <div className="container" >
+   <TextForm></TextForm>
+  
+   </div>
+  
    </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm() {
+    const onclickHandle=()=>
+    {
+        console.log("clicked on button")
+        let newText= text.toUpperCase();
+        setText(newText);
+    }
+
+    const changeHandle=(event)=>
+    {
+        setText(event.target.value);
+    }
+
+    const[text,setText]=useState("enter your text");
+  return (
+  <>
+  <div className="mb-3">
+    <h1>Enter text here:</h1>
+    <textarea className="form-control" value={text} onChange={changeHandle} id="boxarea" rows="3"></textarea>
+  </div>
+   <button className="btn btn-primary" onClick={onclickHandle}>Convert to upper case</button>
+   </>
+  )
+}
