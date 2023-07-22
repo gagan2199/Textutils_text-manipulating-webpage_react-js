be4ffe8617905729c89635f92902fcb8d9480f48
Gagan <gagank2199@gmail.com>
2023-07-22 23:49:52 +0530

    dark mode

--- src/components/About.js
+++ src/components/About.js
@@ -58,7 +58,6 @@ export default function About() {
                   is shown by default, until the collapse plugin adds the
                   appropriate classes that we use to style each element. These
                   classes control the overall appearance, as well as the showing
-                  and hiding via CSS transitions. You can modify any of this
                   with custom CSS or overriding our default variables. It's also
                   worth noting that just about any HTML can go within the{" "}
                   <code>.accordion-body</code>, though the transition does limit
