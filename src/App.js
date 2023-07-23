d2de02663f233e2760a2d2e0201d9ea76e55a249
Gagan <gagank2199@gmail.com>
2023-07-23 16:39:14 +0530

    adding dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/Textform';
-import About from './components/About';
+import TextForm from './components/Textform';
+// import About from './components/About';
 
 function App() {
+
+  const[mode,setmode]=useState('light');
+
+  const togMode=()=>
+  {
+    if(mode ==='light')
+    {
+      setmode('dark');
+    }
+    else
+    {
+      setmode('light');
+    }
+  }
   return (
     <>
-   <Navbar title="Textutils" about="About Us"></Navbar>
+   <Navbar title="Textutils" mode={mode} toggleMode={togMode}></Navbar>
    <div className="container" >
-   {/* <TextForm></TextForm> */}
-   <About></About>
+   <TextForm></TextForm>
+   {/* <About></About> */}
    
    </div>
   
